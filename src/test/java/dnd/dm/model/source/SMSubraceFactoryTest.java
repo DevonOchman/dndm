f8bb45fa42c17cc8329ba42bc1f31e7d35f0cd87
Devon Ochman <Devon Ochman@DESKTOP-DNTP48F>
2017-10-19 00:38:33 -0400

    Added Tests for all sm factories

---
+++ src/test/java/dnd/dm/model/source/SMSubraceFactoryTest.java
@@ -0,0 +1,42 @@
+package dnd.dm.model.source;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = { dnd.dm.model.source.SMLevelFactoryTest.class })
+public class SMSubraceFactoryTest {
+
+	private File sourceFile = null;
+
+	private SMFactory subject = SMSubraceFactory.getInstance();
+
+	@Before
+	public void init() {
+		sourceFile = new File(
+				getClass().getClassLoader().getResource("ModelSource/" + subject.getSourceFileName()).getFile());
+		sourceFile = new File(sourceFile.getPath().replaceAll("%20", " "));
+		assertNotNull(sourceFile);
+	}
+
+	@Test
+	public void testLoadSMObjects() throws JsonParseException, JsonMappingException, IOException {
+		subject.loadSMObjects(sourceFile);
+		assertFalse(subject.isEmpty());
+		assertNotEquals(0, subject.size());
+		// TODO more checks against contents
+	}
+}
